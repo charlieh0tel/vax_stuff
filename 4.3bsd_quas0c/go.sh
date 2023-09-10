37b3bc6e2146d7be0f82c8243af42ef996e955e5
Christopher Hoover <ch@murgatroid.com>
2023-09-09 21:31:04 -0700

    works

--- 4.3bsd_quas0c/go.sh
+++ 4.3bsd_quas0c/go.sh
@@ -3,11 +3,15 @@
 set -o errexit
 set -o nounset
 
+make
+
 rm -f quas.ra92
 ./mkdisk.sh
 
 ./00_install.expect
-cp -p quas.ra92 quas.ra92.post00
+cp -p quas.ra92 quas.ra92.post00_snap
 
 ./01_install.expect
-cp -p quas.ra92 quas.ra92.post01
+cp -p quas.ra92 quas.ra92.post01_snap
+
+exit 0
